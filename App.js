0ea06f87d454cffe3dbd2192c3bd938124ecd284
antvega <antvega627@gmail.com>
2023-12-06 19:54:00 -0800

    Added swap function when scrambling alphabet.

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default function App() {
+  const [alphabet, setAlphabet] = useState("abcdefghijklmnopqrstuvwxyz");
+
+  const scrambleAlphabet = () => {
+    const arr = [...alphabet];
+    const scrambled = [];
+    while (arr.length > 0) {
+      arr.swap(arr, getRandomIndex(arr.length), arr.length - 1);
+      scrambled.push(arr.pop());
+    }
+    setAlphabet(scrambled.join(""));
+  };
+
+  const getRandomIndex = (len) => {
+    return Math.floor(Math.random() * len);
+  };
+
+  const swap = (arr, i, j) => {
+    const copy = arr[i];
+    arr[i] = arr[j];
+    arr[j] = copy;
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Alphabet: 'abcdefghijklmnopqrstuvwxyz'</Text>
+      <Text>Scrambled Alphabet: {alphabet}</Text>
+      <TouchableOpacity
+        style={styles.scrambleButton}
+        onPress={() => {
+          scrambleAlphabet();
+        }}
+      >
+        <Text style={{ fontSize: 18 }}>Scramble</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +44,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  scrambleButton: {
+    borderRadius: 10,
+    padding: 10,
+    backgroundColor: "lightblue",
   },
 });
